37ddf27177dfbde5b0820899372ba50f0b213610
Leyton Reed <leyton@leyton.xyz>
2019-02-07 19:59:43 +0000

    Update cloudflare.sh

--- cloudflare.sh
+++ cloudflare.sh
@@ -1,4 +1,4 @@
-curl -X POST "https://api.cloudflare.com/client/v4/zones/e676aa7dfb44071384986c3b8e84a14d/purge_cache" \
+curl -sX POST "https://api.cloudflare.com/client/v4/zones/e676aa7dfb44071384986c3b8e84a14d/purge_cache" \
     -H "X-Auth-Email: $CLOUDFLARE_API_USER" \
     -H "X-Auth-Key: $CLOUDFLARE_API_TOKEN" \
     -H "Content-Type: application/json" \
